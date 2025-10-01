579a6f1793fae95f9052918f6e5260ca69415d09
Mained606 <mained606@gmail.com>
2025-10-01 18:02:24 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251001/특정 수 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n[10];
+    int sum = 0;
+
+    cout << fixed;
+    cout.precision(1);
+
+    for(int i = 0; i < 10; i++) {
+        cin >> n[i];
+        if(n[i] >= 250) {
+            cout << sum << " ";
+            cout << (float)sum / i;
+            break;
+        }
+        else {
+            sum += n[i];
+        }
+    }
+    return 0;
+}
